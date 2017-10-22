7d067df7bf51ba589a9cadbaf02856a2ba867022
John Deng <john.deng@qq.com>
2017-10-22 08:56:28 +0800

    corrected hi.conf

---
+++ simple-product-command/src/main/java/com/example/simpleproductcommand/config/AxonConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.simpleproductcommand.config;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.amqp.eventhandling.spring.SpringAMQPMessageSource;
+import org.axonframework.serialization.Serializer;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@Slf4j
+public class AxonConfiguration {
+    // listen to queue
+    @Bean
+    public SpringAMQPMessageSource springAMQPMessageSource(Serializer serializer) {
+        return new SpringAMQPMessageSource(serializer) {
+            @RabbitListener(queues = "moses-simpleproduct-command")
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+                log.info("message received: " + message.toString());
+                super.onMessage(message, channel);
+            }
+        };
+    }
+}
